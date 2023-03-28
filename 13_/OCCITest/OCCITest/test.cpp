0ac97bf5ecd27a3d39d34d63bf9821b879eba2ad
12313 <zcsada@11.com>
2023-03-28 19:34:11 +0800

    23.3.28 19.34

--- 13_/OCCITest/OCCITest/test.cpp
+++ 13_/OCCITest/OCCITest/test.cpp
@@ -6,5 +6,12 @@ using namespace oracle::occi;
 int main()
 {
 	Environment* env = Environment::createEnvironment();
+	//创建对象
+	Connection* conn = env->createConnection("SCOTT","123456",);
+
+	//销毁对象
+	env->terminateConnection(conn);
+	//释放环境对象
+	Environment::terminateEnvironment(env);
 	return 0;
 }
